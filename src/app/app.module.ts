073ecd74fde2fdae5a02d9c2588b874af26659e6
StefanyMikaelle <stefany_mikaelle@hotmail.com>
2024-02-06 00:00:55 -0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule
